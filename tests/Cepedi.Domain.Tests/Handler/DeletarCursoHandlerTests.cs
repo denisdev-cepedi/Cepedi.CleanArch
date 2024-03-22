1a86a60cb799243922974a140428be9cd889ccaa
Beatriz-ux <beatrizpereira.ld@gmail.com>
2024-03-22 09:14:25 -0300

    feat: test DeletarCurso

---
+++ tests/Cepedi.Domain.Tests/Handler/DeletarCursoHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using Cepedi.Domain.Entities;
+using NSubstitute;
+
+namespace Cepedi.Domain.Tests;
+
+public class DeletarCursoHandlerTests
+{
+    private readonly ICursoRepository _cursoRepository = Substitute.For<ICursoRepository>();
+
+    private readonly DeletarCursoHandler _sut;
+
+    public DeletarCursoHandlerTests (){
+        _sut = new DeletarCursoHandler(_cursoRepository);
+    }
+
+    [Fact]
+    public async Task DeletarCurso_PassandoIDExistente_DeveRetornar1 (){
+        // Arrange
+        int id = 1;
+        var curso = new CursoEntity
+        {
+            Id = 1,
+            Nome = "nomeDel",
+            Descricao = "descricao",
+            DataInicio = DateTime.Now,
+            DataFim = DateTime.Now,
+            ProfessorId = 1,
+        };
+        _cursoRepository.ObtemCursoPorIdAsync(id).ReturnsForAnyArgs(curso);
+        _cursoRepository.DeletarCursoAsync(curso).ReturnsForAnyArgs(1);
+
+        //act
+        var result = await _sut.DeletarCursoAsync(id);
+        
+        //assets
+        Assert.Equal(1 ,result );
+    }
+}
