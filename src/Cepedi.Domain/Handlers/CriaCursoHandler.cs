5b794d34d919777fc32196d6708dc4c55031c665
Lorena Andrade <lorenaandrade@gmail.com>
2024-04-08 15:37:39 -0300

    mediator

--- src/Cepedi.Domain/Handlers/CriaCursoHandler.cs
+++ src/Cepedi.Domain/Handlers/CriaCursoHandler.cs
@@ -5,31 +5,39 @@ using System.Threading.Tasks;
 using Cepedi.Domain.Entities;
 using Cepedi.Domain.Repository;
 using Cepedi.Shareable.Requests;
+using Cepedi.Shareable.Responses;
+using MediatR;
 
-namespace Cepedi.Domain.Handlers
-{
-    public class CriaCursoHandler: ICriaCursoHandler
+namespace Cepedi.Domain.Handlers;
+
+    public class CriarCursoRequestHandler: IRequestHandler<CriarCursoRequest, CriarCursoResponse>
     {
          private readonly ICursoRepository _cursoRepository;
 
-        public CriaCursoHandler(ICursoRepository cursoRepository)
+        public CriarCursoRequestHandler(ICursoRepository cursoRepository)
         {
             _cursoRepository = cursoRepository;
         }
 
-        public async Task<int> CriarCursoAsync(CriaCursoRequest request)
+        public Task<CriarCursoResponse> Handle(CriarCursoRequest request, CancellationToken cancellationToken)
         {
-            var novoCurso = new CursoEntity
+            try
             {
-                Nome = request.Nome,
-                Descricao = request.Descricao,
-                DataInicio = request.DataInicio,
-                DataFim = request.DataFim,
-                ProfessorId = request.ProfessorId
-            };
+                var novoCurso = new CursoEntity()
+                {
+                    Nome = request.Nome,
+                    Descricao = request.Descricao,
+                    DataInicio = request.DataInicio,
+                    DataFim = request.DataFim,
+                    ProfessorId = request.ProfessorId
+                };
+                await _cursoRepository.CriaNovoCursoAsync(novoCurso);
 
-            return await _cursoRepository.CriaNovoCursoAsync(novoCurso);
+                return new CriarCursoResponse(novoCurso.idCurso, novoCurso.Nome);
+            catch
+            {
+                throw;
+            }
         }
-
     }
 }
---
+++ src/Cepedi.Shareable/Responses/CriarCursoResponse.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cepedi.Shareable.Responses;
+
+public record CriarCursoResponse(string idCurso, string Nome);
