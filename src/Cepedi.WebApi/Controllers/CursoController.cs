448d26a06cfe11ce25e5bb1da25c2364da1c25cb
Marcelo da Silva Cruz <mcruz@uesc.br>
2024-04-08 16:20:40 -0300

    Atualização mediador e appsettings

--- src/Cepedi.WebApi/Controllers/CursoController.cs
+++ src/Cepedi.WebApi/Controllers/CursoController.cs
@@ -1,9 +1,12 @@
-﻿using Cepedi.Domain;
+﻿﻿using Cepedi.Domain;
 using Cepedi.Domain.Repository;
 using Cepedi.IoC;
+using Cepedi.Shareable.Exceptions;
 using Cepedi.Shareable.Requests;
 using Cepedi.Shareable.Responses;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace Cepedi.WebApi.Controllers;
 
@@ -12,24 +15,30 @@ namespace Cepedi.WebApi.Controllers;
 public class CursoController : ControllerBase
 {
     private readonly ILogger<CursoController> _logger;
-    private readonly IObtemCursoHandler _obtemCursoHandler;
+    private readonly IMediator _mediator;
+    
+
+/*     private readonly IObtemCursoHandler _obtemCursoHandler;
 
     private readonly ICriaCursoHandler _criaCursoHandler;
-    private readonly IAlteraCursoHandler _alteraCursoHandler;
+    private readonly IAlteraCursoHandler _alteraCursoHandler; */
 
     public CursoController(
         ILogger<CursoController> logger,
-        IObtemCursoHandler obtemCursoHandler,
+        IMediator mediator
+/*         IObtemCursoHandler obtemCursoHandler,
         ICriaCursoHandler criaCursoHandler,
-        IAlteraCursoHandler alteraCursoHandler)
+        IAlteraCursoHandler alteraCursoHandler) */
+    )
     {
         _logger = logger;
-        _obtemCursoHandler = obtemCursoHandler;
+        _mediator = mediator;
+/*         _obtemCursoHandler = obtemCursoHandler;
         _criaCursoHandler = criaCursoHandler;
-        _alteraCursoHandler = alteraCursoHandler;
+        _alteraCursoHandler = alteraCursoHandler; */
     }
 
-    [HttpGet("{idCurso}")]
+/*     [HttpGet("{idCurso}")]
     public async Task<ActionResult<ObtemCursoResponse>> ConsultarCursoAsync([FromRoute] int idCurso)
     {
         return Ok(await _obtemCursoHandler.ObterCursoAsync(idCurso));
@@ -38,17 +47,20 @@ public class CursoController : ControllerBase
     public async Task<ActionResult<IEnumerable<ObtemCursoResponse>>> ConsultarCursosAsync()
     {
         return Ok(await _obtemCursoHandler.ObterCursosAsync());
-    }
+    } */
     [HttpPost]
-    public async Task<ActionResult<int>> CriarCursoAsync([FromBody] CriaCursoRequest request)
+    [ProducesResponseType(typeof(CriarCursoResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseError), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<CriarCursoResponse>> CriarCursoAsync([FromBody] CriarCursoRequest request)
     {
-        var cursoId = await _criaCursoHandler.CriarCursoAsync(request);
-        return Ok(cursoId);
+        /* var cursoId = await _criaCursoHandler.CriarCursoAsync(request);
+        return Ok(cursoId); */
+        return await _mediator.Send(request);
     }
-    [HttpPut]
+/*     [HttpPut]
     public async Task<ActionResult<int>> AlterarCursoAsync([FromBody] AlteraCursoRequest request)
     {
         var cursoId = await _alteraCursoHandler.AlterarCursoAsync(request);
         return Ok(cursoId);
-    }
+    } */
 }
