5b74d934cd3a39a02f74c66161e6773c9de268e7
kayquepiton <kayquepiton@gmail.com>
2024-04-09 12:03:04 -0300

    controller

--- src/Cepedi.WebApi/Controllers/CursoController.cs
+++ src/Cepedi.WebApi/Controllers/CursoController.cs
@@ -1,61 +1,57 @@
-﻿using Cepedi.Domain;
-using Cepedi.Domain.Repository;
-using Cepedi.IoC;
+﻿using Cepedi.Data;
+using Cepedi.Domain;
+using Cepedi.Shareable.Exceptions;
 using Cepedi.Shareable.Requests;
 using Cepedi.Shareable.Responses;
-using MediatR; 
-using Microsoft.AspNetCore.Http;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace Cepedi.WebApi.Controllers
+namespace Cepedi.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CursoController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class CursoController : ControllerBase
+    private readonly ILogger<CursoController> _logger;
+
+    private readonly IMediator _mediator;
+    public CursoController(ILogger<CursoController> logger, IMediator mediator)
     {
-        private readonly ILogger<CursoController> _logger;
-        private readonly IMediator _mediator;
-
-        public CursoController(
-            ILogger<CursoController> logger,
-            IMediator mediator)
-        {
-            _logger = logger;
-            _mediator = mediator;
-        }
-
-        /*[HttpGet("{idCurso}")]
-        public async Task<ActionResult<ObtemCursoResponse>> ConsultarCursoAsync([FromRoute] int idCurso)
-        {
-            var curso = await _obtemCursoHandler.ObterCursoAsync(idCurso);
-            if (curso == null)
-                return NotFound();
-
-            return Ok(curso);
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ObtemCursoResponse>>> ConsultarCursosAsync()
-        {
-            var cursos = await _obtemCursoHandler.ObterCursosAsync();
-            return Ok(cursos);
-        }*/
-
-        [HttpPost]
-        public async Task<ActionResult<int>> CriarCursoAsync([FromBody] CriaCursoRequest request)
-        {
-            var cursoId = await _mediator.Send(request);
-            return Ok(cursoId);
-        }
-
-        /*[HttpPut]
-        public async Task<ActionResult<int>> AlterarCursoAsync([FromBody] AlteraCursoRequest request)
-        {
-            var cursoId = await _mediator.Send(request);
-            return Ok(cursoId);
-        }*/
+        _logger = logger;
+        _mediator = mediator;
     }
+
+    // [HttpGet("{idCurso}")]
+    // public async Task<ActionResult<ObtemCursoResponse>> ConsultarCursoAsync([FromRoute] int idCurso)
+    // {
+    //     return Ok(await _obtemCursoHandler.ObterCursoAsync(idCurso));
+    // }
+    // [HttpGet]
+    // public async Task<ActionResult<IEnumerable<ObtemCursoResponse>>> ObterCursosAsync()
+    // {
+    //     var cursos = await _obtemCursoHandler.ObterCursosAsync();
+    //     return Ok(cursos);
+    // }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ObtemCursoResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<int>> CriarCursoAsync([FromBody] CriarCursoRequest request)
+    {
+        var cursoId = await _mediator.Send(request);
+        return Ok(cursoId);
+    }
+    // [HttpPut]
+    // public async Task<ActionResult<int>> AlterarCursoAsync([FromBody] AlteraCursoRequest request)
+    // {
+    //     var cursoId = await _alteraCursoHandler.AlterarCursoAsync(request);
+    //     return Ok(cursoId);
+    // }
+    // [HttpDelete("{idCurso}")]
+    // public async Task<ActionResult<int>> ExcluirCursoAsync([FromRoute] int idCurso)
+    // {
+    //     var cursoId = await _excluirCursoHandler.ExcluirCursoAsync(idCurso);
+    //     return Ok(cursoId);
+    // }
 }
