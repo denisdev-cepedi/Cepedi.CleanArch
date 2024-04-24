d23b49e572b57af85a4faa504e47225c73ee178d
AlanPrates <alan_nz@live.com>
2024-04-24 17:49:02 -0300

    Adiciona nome do remetente às mensagens enviadas pelo RabbitMQ

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "srv508250.hstgr.cloud",
+            UserName = "aluno",
+            Password = "changeme"
+        };
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        {
+            channel.QueueDeclare(queue: "Alan Prates",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            string message = "Teste de envio de mensagem! Alan Prates";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "",
+                routingKey: "pratestarrk",
+                basicProperties: null,
+                body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+        
+
+    }
+}
