9b0b9c8d9284f2427be2ff71d9d4192328c5b88b
kayquepiton <kayquepiton@gmail.com>
2024-04-24 22:42:03 -0300

    FilaRabbitMQ

--- WebApplication-RabbitMQ/Program.cs
+++ WebApplication-RabbitMQ/Program.cs
@@ -1,35 +1,56 @@
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
+using RabbitMQ.Client; // Importa o namespace necessário para interagir com o RabbitMQ
 using System;
 using System.Text;
 
 class Program
 {
-     void sendMessage()
+    static void Main(string[] args)
+    {
+        SendMessage(); // Chama o método para enviar uma mensagem
+    }
+
+    static void SendMessage()
+    {
+        try
         {
+            // Configuração da fábrica de conexão com o RabbitMQ
+            var factory = new ConnectionFactory()
+            {
+                HostName = "srv508250.hstgr.cloud", // Hostname do servidor RabbitMQ
+                UserName = "aluno", // Nome de usuário para autenticação
+                Password = "changeme", // Senha para autenticação
+                Port = 5672 // Porta padrão do RabbitMQ
+            };
 
-            var factory = new ConnectionFactory() { HostName = "srv508250.hstgr.cloud", UserName = "aluno", Password = "changeme", Port = 5672 };
-
+            // Criação e utilização da conexão e do canal (channel) RabbitMQ
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "Fila.Teste.Welvis",
+                // Declaração da fila no RabbitMQ
+                channel.QueueDeclare(queue: "FilaRabbitMQ.Kayque",
                                      durable: false,
                                      exclusive: false,
                                      autoDelete: false,
                                      arguments: null);
 
-                string message = "Hello World Welvis!";
+                string message = "Hasta la vista, baby";
                 var body = Encoding.UTF8.GetBytes(message);
 
+                // Publica a mensagem na fila especificada
                 channel.BasicPublish(exchange: "",
-                                     routingKey: "Fila.Teste.Welvis",
+                                     routingKey: "FilaRabbitMQ.Kayque",
                                      basicProperties: null,
                                      body: body);
+
                 Console.WriteLine(" [x] Sent {0}", message);
 
                 Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
+                Console.ReadLine(); // Aguarda a entrada do usuário antes de sair
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}"); // Exibe mensagem de erro em caso de exceção
+        }
+    }
 }
